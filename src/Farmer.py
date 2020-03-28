6f1434daf3e3345a55865942c3721f824d692c1c
SzymonDomzalski <49538030+SzymonDomzalski@users.noreply.github.com>
2020-03-28 16:02:18 +0100

    Update Farmer.py

--- src/Farmer.py
+++ src/Farmer.py
@@ -3,6 +3,9 @@ class Farmer(object):
         self.x = x
         self.y = y
         self.capacity = 3
+        self.state = 0
+        self.targetY= null
+        self.targetX= null
 
     def getX(self):
         return self.x
@@ -22,8 +25,57 @@ class Farmer(object):
     def setCapacity(self, capacity):
             self.capacity = capacity
 
+    def gettargetX(self):
+        return self.targetX
+
+    def settargetX(self, x):
+        self.targetX = x
+
+    def gettargetY(self):
+        return self.targetY
+
+    def settargetY(self, y):
+        self.targetY = y
+
     def pickFood(self):
         self.capacity = 3 #later add parametr with amount of food to pick up
 
     def feed(self):
         self.capacity = self.capacity-1
+    def targets(self, pigs):
+        if self.state == 0:
+            if self.capacity == 0:
+                self.settargetX(0)
+                self.settargetY(0)
+                self.state = 1
+            else:
+                for pig in pigs:
+                    if pig.hungry == True:
+                        self.settargetY(pig.getY)
+                        self.settargetX(pig.getX)
+                        self.state = 2
+                        break
+    def move(self):
+        if self.state == 1:
+            self.y -= 1
+            if self.targetY == self.y:
+                self.pickFood()
+                self.state = 0
+        elif self.state == 2:
+            if self.y == self.targetY:
+                self.state = 0
+            elif self.y > self.targetY:
+                self.y -= 1
+                if self.y == self.targetY:
+                    self.feed()
+                    self.state = 0
+            else:
+                self.y += 1
+                if self.y == self.targetY:
+                    self.feed()
+                    self.state = 0
+
+
+
+
+
