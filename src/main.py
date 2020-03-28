bc1261f16be45662d2ca93279cf94162d774403a
SirLeni87 <t.grochowski1@onet.pl>
2020-03-28 15:13:22 +0100

    first draft of main function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from src.Pig import Pig
 from src.Farmer import Farmer
+from src.grid import Grid
+import time
 
 
 def main():
@@ -10,6 +12,21 @@ def main():
                 pig = Pig(j, k)
                 pigs.append(pig)
     farmer = Farmer(3,0)
+
+    grid = Grid(3,5)
+    grid.putin(0,0,'O')
+    grid.putin(0,1,'F')
+    for i in range(0,3,2):
+        for j in range(1,5):
+            grid.putin(j,i,'P')
+
+    running = True
+
+    while running:
+        grid.printgrid()
+        time.sleep(1)
+        running = False
+
 
 
 if __name__ == "__main__":
