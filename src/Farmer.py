985352a1a083f70960cff1310473967d1224de30
Radekowicz <radeklodzinski@gmail.com>
2020-03-28 14:36:29 +0100

    farmer added

--- src/Farmer.py
+++ src/Farmer.py
@@ -1,4 +1,4 @@
-class Farmer():
+class Farmer:
     def __init__(self, x, y):
         self.x = x
         self.y = y
--- src/Food.py
+++ src/Food.py
@@ -1,4 +1,4 @@
-class Food():
+class Food:
 
     def __init__(self, x, y, amount):
         self.x = x
