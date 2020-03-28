3ff46ed0af04c858b301792fbecb124a415e7793
SirLeni87 <t.grochowski1@onet.pl>
2020-03-28 14:30:35 +0100

    Pig update and creating Pig array in main

--- src/Pig.py
+++ src/Pig.py
@@ -1,17 +1,18 @@
+import random
 
 
-class Pig():
+class Pig(object):
 
-    def __init__(self, hunger = 100, x = 0, y = 0, hungry = False):
-        self.hunger = hunger
+    def __init__(self, x, y):
+        self.hunger = 100
         self.x = x
         self.y = y
-        self.hungry = hungry
+        self.hungry = False
 
     def isStarving(self):
         if self.hunger < 50:
             self.hungry = True
 
-    def getFed(self):
-        print("TODO")
+    def getHungry(self):
+        self.hunger = self.hunger - random.randrange(0, 5, 1)
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+from src.Pig import Pig
 
 
 def main():
-    print("Hello world!")
-    print("hehe")
+    pigs = []
+    for i in range(0,8,1):
+        for j in range(1,3,2):
+            for k in range(1,4,1):
+                pig = Pig(j, k)
+                pigs.append(pig)
 
 
 if __name__ == "__main__":
