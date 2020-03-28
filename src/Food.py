7895e3c17d39c30d5dc89809c5fe7d619bfbaf1a
Radekowicz <radeklodzinski@gmail.com>
2020-03-28 13:41:07 +0100

    getter and setter for food

---
+++ src/Food.py
@@ -0,0 +1,17 @@
+class Food():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+
+    def setX(self, x):
+        self.x = x
+
+    def getY(self):
+        return self.y
+
+    def setY(self, y):
+        self.y = y
